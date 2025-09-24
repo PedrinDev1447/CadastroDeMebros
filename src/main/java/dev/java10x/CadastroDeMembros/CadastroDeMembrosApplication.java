ab8d5f3a18fc0bbe5a6613d195ec32f9de78d07e
PEDRO LUCAS <pedrolucas.nft2021@gmail.com>
2025-09-24 16:06:08 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroDeMembros/CadastroDeMembrosApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeMembros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeMembrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeMembrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeMembros/CadastroDeMembrosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeMembros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeMembrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
